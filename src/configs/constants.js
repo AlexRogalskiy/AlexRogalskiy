139295d8409040aa03719b72e4b55aabde1016dd
ADMSK\AVROGAL1 <avrogal1@mts.ru>
2021-01-31 17:56:50 +0300

    docs: updates on workflows
    Code refactoring and workflow optimization

---
+++ src/configs/constants.js
@@ -0,0 +1,18 @@
+const CONFIG_PROPS = {
+    "target": "README.md"
+}
+
+const SERVICE_PROPS = {
+    INSTAGRAM: {
+        "number": 3,
+        "tag": "visitpetersburg",
+        "view": "./src/views/photo-marker.view.mustache",
+        "placeholder": /<!--views:photo-marker:start-->[\s\S]*?<!--views:photo-marker:end-->/gm,
+        "replacer": (value) => '<!--views:photo-marker:start-->\n' + value + '\n<!--views:photo-marker:end-->'
+    }
+}
+
+module.exports = {
+    CONFIG_PROPS,
+    SERVICE_PROPS
+}
